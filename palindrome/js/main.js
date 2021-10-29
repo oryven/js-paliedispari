8f1b230a6d95165a89e3c59807d1144b12caeed6
Orazio <orazioventrella@hotmail.it>
2021-10-29 16:02:32 +0200

    prima analisi

---
+++ palindrome/js/main.js
@@ -0,0 +1,10 @@
+ /* Chiedere all’utente di inserire una parola
+    Creare una funzione per capire se la parola inserita è palindroma.
+    Dare l’output relativo. 
+*/
+
+
+// 1. chiedere all'utente di scrivere una parola
+// 2. invertire l'ordine delle lettere per capire sa la parola è palindroma
+// 3. stampare il risultato
+
