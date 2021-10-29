560a49d722a893033c406f9cb585e6e378952b02
Orazio <orazioventrella@hotmail.it>
2021-10-29 17:32:55 +0200

    esercizio 1

--- palindrome/js/main.js
+++ palindrome/js/main.js
@@ -4,7 +4,27 @@
 */
 
 
-// 1. chiedere all'utente di scrivere una parola
+// 1. chiedere all'utente di scrivere una parola => promnt
 // 2. invertire l'ordine delle lettere per capire sa la parola è palindroma
 // 3. stampare il risultato
 
+
+let parola = prompt("scrivi una parola");
+
+if (controlloPalindroma(parola) == parola){
+    console.log("la tua parola è palindrma");
+}else {
+    console.log("la tua parola non è palindroma");
+}
+
+function controlloPalindroma(parolaInserita){
+
+    let parolaInvertita = "";
+
+    for (let i = parolaInserita.length - 1; i >= 0; i--) {
+        parolaInvertita += parolaInserita[i];
+        console.log(parolaInvertita);
+    }
+
+    return parolaInvertita;
+}
