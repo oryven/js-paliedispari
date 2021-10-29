0dc05e8aaaf853dd63f816e792b0e491b3f7424a
Orazio <orazioventrella@hotmail.it>
2021-10-29 18:22:06 +0200

    analisi

---
+++ pari o dispari/js/main.js
@@ -0,0 +1,15 @@
+/*
+    L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    Sommiamo i due numeri
+    Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+    Dichiariamo chi ha vinto.
+
+
+    1. chiedere all'utente di scegliere pari o dispari
+    2. chiedere all'utente numero 
+    3. genero un numero random da 1 a 5 per il computer
+    4  sommo i numeri
+       4.1 verificare che la somma sia pari o dispari
+    5. stampare il risultato
+*/
